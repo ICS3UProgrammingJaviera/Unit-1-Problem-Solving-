080dc9f999622c19d808991768794fc75208a2b1
Javiera Diaz <s455309@ocsb.local>
2018-09-21 10:47:52 -0400

    Added CircumfJaviera folder

---
+++ CircumfJaviera/CircumfJaviera/CircumferenceForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircumfJaviera
+{
+    public partial class frmCircumference : Form
+    {
+        public frmCircumference()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
