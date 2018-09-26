439f26228c7a79f53463ef082ec21dcc99b6821e
17JavieraDiaz <43445003+17JavieraDiaz@users.noreply.github.com>
2018-09-25 20:43:40 -0400

    Add files via upload

---
+++ FallingObjectsJav/FallingObjectsJav/FallingObjectsForm.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by: Javiera
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Assignment #2 - Name of Program
+ * This program calculates how close a falling object is to the ground from 100m with a varible height given by the user.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjectsJav
+{
+    public partial class frmFallingObjects : Form
+    {
+        public frmFallingObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void FallingObjectsForm_Load(object sender, EventArgs e)
+        {
+            //Hide these labels
+            this.lblAnswer.Hide();
+            this.lblStatment.Hide();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //close the program
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //declare variables
+            double time, height;
+
+            //convert time(textbox text) to a string
+            time = double.Parse(txtTime.Text);
+
+            //calculate formula
+            height = 100 - 0.5 * 9.81 * Math.Pow(time, 2);
+
+            //insert respectable labels to textbox
+            this.lblAnswer.Text = Convert.ToString(height) + "m";
+
+            //Show labels when calculate button is clicked
+            this.lblAnswer.Show();
+            this.lblStatment.Show();
+        }
+    }
+}
---
+++ FallingObjectsJav/FallingObjectsJav/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FallingObjectsJav")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FallingObjectsJav")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45e3df4a-a66b-4761-95c1-bc9739fa867e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
